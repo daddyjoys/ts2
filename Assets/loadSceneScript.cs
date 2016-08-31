2543f2fcaf69faed2d48c29cc8366d4f5ba6d62a
Xapac <xapac@list.ru>
2016-09-01 00:37:17 +0300

    Сделал меню загрузки уровня close #8

---
+++ Assets/loadSceneScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loadSceneScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("startLEvel");
+	}
+	
+	IEnumerator startLEvel(){
+		AsyncOperation async = Application.LoadLevelAsync("test");
+		while(!async.isDone){
+			float p = async.progress * 100f;
+			int r = Mathf.RoundToInt(p);
+			Debug.Log("Загрузка ... " + r);
+			yield return true; 
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/script/levelMenuScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class levelMenuScript : MonoBehaviour {
+
+	public void loadLevel(int numLevel){
+		Application.LoadLevel("loadScene");
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
